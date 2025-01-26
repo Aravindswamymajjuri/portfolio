5a099298fdf2708f0b01067f47384175f2de6f6f
Aravindswamymajjuri <aravindswamymajjuri143@gmail.com>
2025-01-26 15:51:54 +0530

    add app.js

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import About from './components/about';
+import Projects from './components/projects';
+import Skills from './components/skills';
+import Contact from './components/contact';
+import Footer from './components/footer';
+import './App.css';
+import Education from './components/eudcation';
+
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    document.body.className = darkMode ? 'dark-mode' : '';
+  }, [darkMode]);
+
+  return (
+    <div className={`app ${darkMode ? 'dark-mode' : ''}`}>
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Hero />
+      <About />
+      <Education/>
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
